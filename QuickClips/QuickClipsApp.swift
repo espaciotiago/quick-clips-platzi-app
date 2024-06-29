61dc384b4246c813866f62fd4e4b5a5348da8409
Santiago Moreno <santiago.moreno@astropay.com>
2024-06-29 10:40:36 -0500

    Initial Commit

---
+++ QuickClips/QuickClipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickClipsApp.swift
+//  QuickClips
+//
+//  Created by Santiago Moreno on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickClipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
