3b73da19c50d884f5438407b5073137f0a6c0764
Tiago Moreno <santiago.moreno@astropay.com>
2024-07-01 16:54:02 -0500

    Added vm test

---
+++ QuickClipsTests/ViewModels/ClipCardViewModelTest.swift
@@ -0,0 +1,13 @@
+//
+//  ClipCardViewModelTest.swift
+//  QuickClipsTests
+//
+//  Created by Tiago on 1/07/24.
+//
+
+import XCTest
+@testable import QuickClips
+
+final class ClipCardViewModelTest: XCTestCase {
+    
+}
