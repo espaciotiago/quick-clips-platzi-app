b21887ecbb90b8b88bae2ba4d903e8f5c33a3d8b
Santiago Moreno <santiago.moreno@astropay.com>
2024-07-01 18:15:52 -0500

    CacheLatestClipsUseCaseTest

--- QuickClipsTests/UseCases/CacheLatestClipsUseCaseTest.swift
+++ QuickClipsTests/UseCases/CacheLatestClipsUseCaseTest.swift
@@ -10,4 +10,33 @@ import XCTest
 
 final class CacheLatestClipsUseCaseTest: XCTestCase {
     
+    func testCacheLatestClips() {
+        let sut = self.createUseCase()
+        let exp = expectation(description: "testCacheLatestClips")
+        var cached = false
+        sut.execute([]) { completed in
+            cached = completed
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+        XCTAssertTrue(cached)
+    }
+    
+    func testCacheLatestClipsError() {
+        let sut = self.createUseCase(cacheCompleted: false)
+        let exp = expectation(description: "testCacheLatestClipsError")
+        var cached = false
+        sut.execute([]) { completed in
+            cached = completed
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+        XCTAssertFalse(cached)
+    }
+    
+    // MARK: - Helpers
+    private func createUseCase(cacheCompleted: Bool = true) -> CacheLatestClipsUseCase {
+        let localCacheLatestClipsDataSource = CacheLatestClipsUnitTestingDataSource(cacheCompleted: cacheCompleted)
+        return CacheLatestClipsUseCase(localCacheLatestClipsDataSource: localCacheLatestClipsDataSource)
+    }
 }
